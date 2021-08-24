627d820e680ac8312da2e6724755ad1059de9caf
hendersonyang <89329691+hendersonyang@users.noreply.github.com>
2021-08-23 19:47:37 -0800

    Commit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   }); 
+
+document.getElementById("notice").style.marginLeft = window.innerWidth
+let windowwidth = window.innerWidth
+windowwidth -= 300
+setInterval(()=>{
+  windowwidth -= 1
+  if (windowwidth < -100) {
+    windowwidth = window.innerWidth
+    return
+  }
+  document.getElementById("notice").style.marginLeft = `${windowwidth}px`
+  console.log(windowwidth)
+}, 25)
