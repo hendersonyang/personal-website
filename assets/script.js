a1204e73f0644abe55ce0e0b8caa507a0731a8d8
hendersonyang <89329691+hendersonyang@users.noreply.github.com>
2021-08-24 16:53:15 -0800

    plane now moves slightly vertical

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,15 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
 
 document.getElementById("notice").style.left = window.innerWidth
 let windowwidth = window.innerWidth
+let windowheight = window.innerHeight/100*4
 setInterval(()=>{
   windowwidth -= 2
+  windowheight += 0.5
   if (windowwidth < -300) {
     windowwidth = window.innerWidth
+    windowheight = window.innerHeight/100*4
     return
   }
   document.getElementById("notice").style.left = `${windowwidth}px`
+  document.getElementById("notice").style.top = `${windowheight}px`
 }, 25)
